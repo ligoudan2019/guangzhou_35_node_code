69676bfd512517268de8536c702451ad49d9ca38
狗蛋 <aaabbbccc@qq.com>
2019-08-01 20:26:34 +0800

    完成跨域的演示

--- day05/07-corss-server.js
+++ day05/07-corss-server.js
@@ -5,3 +5,13 @@ app.listen(9090,()=>{
 })
 app.use('/views',express.static('views'));
 app.use('/assets',express.static('assets'));
+
+
+app.get('/testCross', (req, res) => {
+    // res.send('alert("你好")');
+    // console.log(req.query);
+    let fnName = req.query.fn;
+
+    let arr = [{name:'狗蛋',age:12},{name :'翠花',age : 16}];
+    res.send(`${fnName}(${JSON.stringify(arr)})`);
+});
---
+++ day05/08-timetou-server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.listen(9090,()=>{
+  console.log('http://127.0.0.1:9090');
+})
+app.use('/views',express.static('views'));
+app.use('/assets',express.static('assets'));
+
+app.get('/timeout', (req, res) => {
+  setTimeout(() => {
+    res.send('123');
+  }, 7000);
+});
